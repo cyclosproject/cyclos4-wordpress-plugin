7041578597e4546c5a5c193dc62db67271fdcb4d
Sandra Both <sandrab.git@gmail.com>
2020-01-16 10:29:17 +0100

    Improve Updater and migrate old translations
    Bugfix after Updater class was moved. Migrate old option records containing labels.
    Remove old option records containing error messages, they can no longer be customized.

--- app/Utils/Updater.php
+++ app/Utils/Updater.php
@@ -8,6 +8,8 @@
 
 namespace Cyclos\Utils;
 
+use Cyclos\Configuration;
+
 /**
  * Updater class.
  */
@@ -28,35 +30,41 @@ class Updater {
 	/**
 	 * Changes when updating to version 2.0.0.:
 	 * - Move option records into one condensed option record.
-	 * - Default to use styling on the loginform when the plugin is updated from an older version.
+	 * - Default to use styling on the loginform because the old version included styling on the form.
 	 */
 	protected function update_200() {
-		// If the database contains old-style option records for the plugin, condense them into one.
-		// Also change the default value of using styles on the login form to true if we update from an older version.
+		// Retrieve the old-style option record values.
 		$cyclos_url      = get_option( 'cyclos_url' );
 		$cyclos_admin    = get_option( 'cyclos_adminuser' );
 		$cyclos_token    = get_option( 'cyclos_token' );
 		$cyclos_redirect = get_option( 'cyclos_redirectUrl' ); // Note: this option was never in an official release but it was in the trunk, so people might have it.
 
-		// If the option records are not found, the plugin was newly installed and we don't need to do anything.
+		// If the option records are not found, the plugin was newly installed or never used and we don't need to do anything.
 		// If the option records are found, we do the updating we need.
 		if ( $cyclos_url || $cyclos_admin || $cyclos_token || $cyclos_redirect ) {
 			// The plugin was updated from an older version. Migrate the options now.
 			// Note: while it is quite unlikely an option record new-style exists, we do retrieve and update it.
 			// The update_option() call later on will create the option record if it does not exist yet.
-			$option = get_option( Configuration::CYCLOS_OPTION_NAME, array() );
-			if ( $cyclos_url ) {
-				$option['cyclos_url'] = $cyclos_url;
-			}
-			if ( $cyclos_admin ) {
-				$option['username'] = $cyclos_admin;
-			}
-			if ( $cyclos_token ) {
-				$option['accessclient_token'] = $cyclos_token;
-			}
-			if ( $cyclos_redirect ) {
-				$option['custom_cyclos_url'] = $cyclos_redirect;
-			}
+			$option                       = get_option( Configuration::CYCLOS_OPTION_NAME, array() );
+			$option['cyclos_url']         = $cyclos_url;
+			$option['username']           = $cyclos_admin;
+			$option['accessclient_token'] = $cyclos_token;
+			$option['custom_cyclos_url']  = $cyclos_redirect;
+
+			// The old version of the plugin might have label texts configured in option records. Migrate them as well.
+			$option['name_label']           = get_option( 'cyclos_t_loginName' );
+			$option['password_label']       = get_option( 'cyclos_t_loginPassword' );
+			$option['submit_button']        = get_option( 'cyclos_t_loginSubmit' );
+			$option['forgot_pw_linktext']   = get_option( 'cyclos_t_forgotLink' );
+			$option['forgot_pw_email']      = get_option( 'cyclos_t_forgotEmail' );
+			$option['forgot_pw_captcha']    = get_option( 'cyclos_t_forgotCaptcha' );
+			$option['forgot_pw_newcaptcha'] = get_option( 'cyclos_t_forgotNewCaptcha' );
+			$option['forgot_pw_submit']     = get_option( 'cyclos_t_forgotSubmit' );
+			$option['forgot_pw_cancel']     = get_option( 'cyclos_t_forgotCancel' );
+
+			// Remove all keys with an empty value.
+			$option = array_filter( $option );
+
 			// Set the loginform styling to true by default.
 			$option['style_loginform'] = true;
 
@@ -67,6 +75,41 @@ class Updater {
 			delete_option( 'cyclos_adminuser' );
 			delete_option( 'cyclos_token' );
 			delete_option( 'cyclos_redirectUrl' );
+			delete_option( 'cyclos_t_loginName' );
+			delete_option( 'cyclos_t_loginPassword' );
+			delete_option( 'cyclos_t_loginSubmit' );
+			delete_option( 'cyclos_t_forgotLink' );
+			delete_option( 'cyclos_t_forgotEmail' );
+			delete_option( 'cyclos_t_forgotCaptcha' );
+			delete_option( 'cyclos_t_forgotNewCaptcha' );
+			delete_option( 'cyclos_t_forgotSubmit' );
+			delete_option( 'cyclos_t_forgotCancel' );
+
+			// Also delete old options we no longer use. These are not migrated, just removed.
+			delete_option( 'cyclos_t_loginTitle' );
+			delete_option( 'cyclos_t_forgotTitle' );
+			delete_option( 'cyclos_t_forgotDone' ); // This message is no longer customizable.
+			// Note: there may also be texts for error messages, but we don't migrate those. We only remove their option records if they exist.
+			// In the new version of the plugin, error messages can no longer be customized, only translated via translate.wordpress.org.
+			delete_option( 'cyclos_t_errorLogin' );
+			delete_option( 'cyclos_t_errorAddressBlocked' );
+			delete_option( 'cyclos_t_errorEmailNotFound' );
+			delete_option( 'cyclos_t_errorConnection' );
+			delete_option( 'cyclos_t_errorGeneral' );
+			// Also delete options that might exist if people used the development version that was never offically released.
+			delete_option( 'cyclos_t_errorInaccessibleChannel' );
+			delete_option( 'cyclos_t_errorInaccessiblePrincipal' );
+			delete_option( 'cyclos_t_errorUserBlocked' );
+			delete_option( 'cyclos_t_errorUserDisabled' );
+			delete_option( 'cyclos_t_errorUserPending' );
+			delete_option( 'cyclos_t_errorPasswordIndefinitelyBlocked' );
+			delete_option( 'cyclos_t_errorPasswordTemporarilyBlocked' );
+			delete_option( 'cyclos_t_errorInvalidPassword' );
+			delete_option( 'cyclos_t_errorInvalidAccessClient' );
+			delete_option( 'cyclos_t_errorOperatorWithPendingAgreements' );
+			delete_option( 'cyclos_t_errorEntityNotFound' );
+			delete_option( 'cyclos_t_errorEntityNotFoundUser' );
+			delete_option( 'cyclos_t_errorEntityNotFoundAccessClient' );
 		}
 	}
 
